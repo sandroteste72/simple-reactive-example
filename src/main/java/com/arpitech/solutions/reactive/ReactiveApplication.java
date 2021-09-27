3c7fc81db3bc0a69fe2726db8aa4b4339b200114
sandroteste72 <sandro.teste72@gmail.com>
2021-09-27 04:55:08 -0300

    Add dependencies

--- src/main/java/com/arpitech/solutions/reactive/ReactiveApplication.java
+++ src/main/java/com/arpitech/solutions/reactive/ReactiveApplication.java
@@ -1,13 +1,37 @@
 package com.arpitech.solutions.reactive;
 
-import org.springframework.boot.SpringApplication;
+import io.reactivex.Flowable;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Random;
+import java.util.stream.IntStream;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 @SpringBootApplication
 public class ReactiveApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReactiveApplication.class, args);
+	public static void main(String[] args) throws InterruptedException {
+		Flowable.interval(1, 1, SECONDS)
+				.map(ReactiveApplication::transform)
+				.subscribe(ReactiveApplication::process, ReactiveApplication::dealWithError);
+		SECONDS.sleep(10);
+
+		/*IntStream.of(1,2,3)
+				.forEach(System.out::println);*/
+	}
+
+	public static void dealWithError(Throwable throwable) {
+		throwable.printStackTrace();
+	}
+
+	public static void process(Long number) {
+		System.out.println("Receive number " + number);
+	}
+
+	public static Long transform(Long number) {
+		if(new Random().nextDouble() < 0.3) throw new RuntimeException("Ops!");
+		return number * 2;
 	}
 
 }
