3a32e6bfa39f242162b47f7a8a2cb6108dfdc617
sandroteste72 <sandro.teste72@gmail.com>
2021-09-27 02:08:17 -0300

    first commit

---
+++ src/main/java/com/arpitech/solutions/reactive/ReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.arpitech.solutions.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arpitech/solutions/reactive/ReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arpitech.solutions.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
